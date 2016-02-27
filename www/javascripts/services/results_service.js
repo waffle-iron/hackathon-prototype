8345137044a2904be6479a6c244f93e11112dc2c
Martin Schurig <martin@schurig.pw>
2016-02-27 18:19:13 +0100

    add getResult method

--- www/javascripts/services/results_service.js
+++ www/javascripts/services/results_service.js
@@ -7,12 +7,32 @@ angular.module('hackathon')
       searchResults = resultsObj.hits;
     }
 
+    function getResult() {
+      return {
+        image: "http://www.tivi.de/imperia/md/images/loewenzahn/p_1er_160x170/artikelsendunglogo.gif",
+        title: "Titel",
+        resource_types: [
+          "Bild",
+          "Simulation",
+          "Werkzeug"
+        ],
+        description: "Description",
+        educational_description: ["Englisch", "Fremdsprachen"],
+        subject: ['Context', 'Context'],
+        age_range: '5-12',
+        rights_de: 'Rights',
+        competence_entry: 'verwenden E-Mail, Chat und Handy zur Kommunikation und beschreiben Vor- und Nachteile der Kommunikationsformen.',
+        publisher: ['Publisher', 'publisher']
+      };
+    }
+
     function getAll() {
       return searchResults;
     }
 
     return {
       add: add,
+      getResult: getResult,
       getAll: getAll
     };
   });
