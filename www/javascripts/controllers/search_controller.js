31c3e955757c9acfa05e888cc8b1000b85cf2705
Martin Schurig <martin@schurig.pw>
2016-02-27 16:41:23 +0100

    getting data from api

--- www/javascripts/controllers/search_controller.js
+++ www/javascripts/controllers/search_controller.js
@@ -1,5 +1,19 @@
 angular.module('hackathon')
-  .controller('SearchController', function($rootScope, $scope, $window, $ionicModal, $localstorage, $state, $ionicHistory) {
-    console.log('searchController..');
+  .controller('SearchController', function($rootScope, $scope, $window, $ionicModal, $localstorage, $state, $ionicHistory, leanlineApiService) {
+    $scope.search = {
+      term: 'Pytagoras',
+      subject: 'Mathematik',
+      class: '5',
+      location: 'Aachen'
+    };
+
+    $scope.performSearch = function search() {
+      leanlineApiService.find($scope.search)
+        .then(function(apiResponse) {
+          console.log(apiResponse.data);
+        }, function(err) {
+          console.log(err);
+        });
+    }
   }
 );
--- www/javascripts/services/leanline_api_service.js
+++ www/javascripts/services/leanline_api_service.js
@@ -3,7 +3,7 @@ angular.module('hackathon')
     function find(query) {
       console.log('TODO: need to add query to url request');
       url = 'http://llnrw.test.publicplan.de/learnline3-rest/rest/e4863bbb-6edb-4995-b2c6-a8a9791bf9d0/search?q=test'
-      return $http.get('https://marcal.cepheus.uberspace.de/nrwhack-php?url=' + url);
+      return $http.get('https://marcal.cepheus.uberspace.de/nrwhack.php?url=' + url);
     }
 
     return {
