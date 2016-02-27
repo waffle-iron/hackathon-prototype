febcbfabecba87d5d99f391f94fb21445bebe831
Roland Awemo <awemo@publicplan.de>
2016-02-27 18:02:58 +0100

    Commit mock data

--- www/javascripts/controllers/result_controller.js
+++ www/javascripts/controllers/result_controller.js
@@ -1,5 +1,20 @@
 angular.module('hackathon')
   .controller('ResultController', function($scope, $state) {
-    $scope.result = {title: "Titel", tags: ['foo', 'bar']};
+    $scope.result = {
+      image: "http://www.tivi.de/imperia/md/images/loewenzahn/p_1er_160x170/artikelsendunglogo.gif",
+      title: "Titel",
+      resource_types: [
+          "Bild",
+          "Simulation",
+          "Werkzeug"
+      ],
+      description: "Description",
+      educational_description: "Edu description",
+      subject: ['Context', 'Context'],
+      age_range: '5-12',
+      rights_de: 'Rights',
+      competence_entry: 'verwenden E-Mail, Chat und Handy zur Kommunikation und beschreiben Vor- und Nachteile der Kommunikationsformen.',
+      publisher: ['Publisher', 'publisher']
+    };
   }
 );
