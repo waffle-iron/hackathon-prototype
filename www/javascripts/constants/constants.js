6035bbbeb50936509d7ff0f918fcc960062daeb1
Roland Awemo <awemo@publicplan.de>
2016-04-06 22:17:48 +0200

    Replace the learnline api endpoint

---
+++ www/javascripts/constants/constants.js
@@ -0,0 +1,4 @@
+angular.module('hackathon')
+  .constant('LEARNLINE_API', {
+    url: 'http://www.learnline.schulministerium.nrw.de/learnline3-rest/rest/e4863bbb-6edb-4995-b2c6-a8a9791bf9d0/search'
+  })
--- www/javascripts/services/leanline_api_service.js
+++ www/javascripts/services/leanline_api_service.js
@@ -1,9 +1,9 @@
 angular.module('hackathon')
-  .factory('leanlineApiService', function($q, $http) {
+  .factory('leanlineApiService', function($q, $http, LEARNLINE_API) {
     function find(searchParams) {
       query = searchParams.term + '&discipline[]=' + searchParams.subject + '&context=' + searchParams.class + '&size=5';
-      url = 'http://llnrw4.test.publicplan.de/learnline3-rest/rest/e4863bbb-6edb-4995-b2c6-a8a9791bf9d0/search?q=' + query;
-      return $http.get('https://marcal.cepheus.uberspace.de/nrwhack.php?url=' + url);
+      url = LEARNLINE_API.url + '?q=' + query;
+      return $http.get(url);
     }
 
     return {
